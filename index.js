c03586254f53cb8cf66dc12e548db89c26804af6
mh akash <mhakash5000@gmail.com>
2023-05-02 11:08:29 +0600

    added food items by id

--- index.js
+++ index.js
@@ -4,9 +4,36 @@ const app = express()
 const port = process.env.PORT||5000;
 app.use(cors())
 
+const categories = require('./data/foods-category.json');
+const food = require('./data/food.json');
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Chef Recipe hunter is running')
 })
+
+app.get('/food',(req,res)=>{
+    res.send(food);
+});
+
+app.get('/food/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectedFood = food.find(f=>f.id === id);
+    res.send(selectedFood)
+})
+
+
+
+app.get('/categories',(req,res)=>{
+res.send(categories)
+});
+
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
