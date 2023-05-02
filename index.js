3578ce1f7740dd788a33032df34cc1cb690c753e
mh akash <mhakash5000@gmail.com>
2023-05-02 08:27:50 +0600

    added route

---
+++ index.js
@@ -0,0 +1,13 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const port = process.env.PORT||5000;
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
